e6b4fdf245aa157c354bc0a6f2e0081187899e74
Marcos Renê <renemoura789@gmail.com>
2023-11-15 11:52:45 -0300

    chore: fix Menu desktop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
@@ -5,24 +6,33 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Button from '../Button'
 import Logo from '../Logo'
+import MediaMatch from '../MediaMatch'
 
 import { MenuProps } from './type'
 
 import * as S from './styles'
-import { useState } from 'react'
 
 const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <S.Wrapper>
-      <S.WrapperIcon onClick={() => setIsOpen(true)}>
-        <MenuIcon aria-label="Open menu" />
-      </S.WrapperIcon>
+      <MediaMatch lessThan="medium">
+        <S.WrapperIcon onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open menu" />
+        </S.WrapperIcon>
+      </MediaMatch>
 
       <S.LogoWrapper>
         <Logo />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
 
       <S.MenuGroup>
         <S.WrapperIcon>
@@ -32,6 +42,12 @@ const Menu = ({ username }: MenuProps) => {
         <S.WrapperIcon>
           <ShoppingCartIcon aria-label="Open shopping cart" />
         </S.WrapperIcon>
+
+        {!!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
