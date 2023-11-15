2803bdb1c2e2a8ff32600a55341a7cc7c7fef70f
Marcos Renê <renemoura789@gmail.com>
2023-11-15 11:43:31 -0300

    feat: add links when user is logged in

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,10 +6,12 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import Button from '../Button'
 import Logo from '../Logo'
 
+import { MenuProps } from './type'
+
 import * as S from './styles'
 import { useState } from 'react'
 
-const Menu = () => {
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
@@ -38,17 +40,26 @@ const Menu = () => {
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My Account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
 
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="#" title="Sign Up">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   )
--- src/components/Menu/type.ts
+++ src/components/Menu/type.ts
@@ -1,3 +1,7 @@
+export type MenuProps = {
+  username?: string
+}
+
 export type StylesProps = {
   isOpen: boolean
 }
