c48044a580de8cbb18468d37ee2bde1009c80a42
Marcos Renê <renemoura789@gmail.com>
2023-11-29 16:28:21 -0300

    feat: add GameCard component

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryFn } from '@storybook/react'
+import styled from 'styled-components'
+
+import { GameCardProps } from './types'
+
+import GameCard from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  },
+  argTypes: {
+    onFav: { action: 'clicked' }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+const Container = styled.div`
+  width: 30rem;
+`
+
+export const Default: StoryFn<GameCardProps> = (args) => (
+  <Container>
+    <GameCard {...args} />
+  </Container>
+)
+
+export const WithPromotionalPrice: StoryFn<GameCardProps> = (args) => (
+  <Container>
+    <GameCard {...args} />
+  </Container>
+)
+
+WithPromotionalPrice.args = {
+  promotionalPrice: 'R$ 200,00'
+}
