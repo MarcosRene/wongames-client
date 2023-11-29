0b071a71a7d47a0864425dde07bd291aad058724
Marcos Renê <renemoura789@gmail.com>
2023-11-29 13:05:00 -0300

    test: Banner + Ribbon component

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,6 +1,7 @@
 import { screen } from '@testing-library/react'
 
 import { renderWithTheme } from '../../utils/tests'
+import theme from '../../styles/theme'
 
 import Banner from '.'
 
@@ -26,5 +27,25 @@ describe('<Banner />', () => {
 
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...bannerProps}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: theme.colors.secondary })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
