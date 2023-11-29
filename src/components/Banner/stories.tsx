ae445c3cff33bfcd531e8d2d93e1068d5f53f0ac
Marcos Renê <renemoura789@gmail.com>
2023-11-29 11:37:22 -0300

    feat: add Benner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Banner from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: StoryObj = {
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+}
---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,75 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+type ImageProps = {
+  src: string
+}
+
+export const Wrapper = styled.main`
+  position: relative;
+
+  ${media.greaterThan('medium')`
+    box-shadow: 0 0.4rem 0.5rem 0 rgba(0, 0, 0, 0.2);
+  `}
+`
+
+export const Image = styled.div<ImageProps>`
+  ${({ theme, src }) => css`
+    width: 100%;
+    height: 23rem;
+    background-color: ${theme.colors.lightGray};
+    background-image: url(${src});
+    background-position: center center;
+    background-size: cover;
+
+    ${media.greaterThan('medium')`
+      height: 58rem;
+    `}
+  `}
+`
+
+export const Caption = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    padding: ${theme.spacings.small};
+    background-color: rgba(0, 0, 0, 0.7);
+
+    ${media.greaterThan('medium')`
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      border-radius: 0 0 ${theme.border.radius} ${theme.border.radius};
+      padding: ${theme.spacings.large};
+    `}
+  `}
+`
+
+export const Title = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.large};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+  `}
+`
+
+export const Subtitle = styled.h3`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.xsmall};
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.normal};
+
+    strong {
+      font-weight: ${theme.font.bold};
+      color: ${theme.colors.primary};
+    }
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.large};
+    `}
+  `}
+`
