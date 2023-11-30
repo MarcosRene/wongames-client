3ff24bba4676fbf18afb02798a6849a83eaac996
Marcos Renê <renemoura789@gmail.com>
2023-11-30 11:33:44 -0300

    test: add BannerSlider component

---
+++ src/components/BannerSlider/types.ts
@@ -0,0 +1,8 @@
+import { BannerProps } from '../Banner/types'
+import { SliderSettings } from '../Slider/types'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+export type SliderSettingsProps = SliderSettings
