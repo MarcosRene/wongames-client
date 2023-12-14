8289e31908f791535086a3459da67a388c0d933b
Marcos Renê <renemoura789@gmail.com>
2023-12-14 11:50:34 -0300

    feat: add Divider component

---
+++ src/components/Divider/test.tsx
@@ -0,0 +1,27 @@
+import { renderWithTheme } from 'utils/tests'
+import { Divider } from '.'
+
+describe('<Divider />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Divider />)
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  height: 0.1rem;
+  margin: 5.6rem auto 3.2rem;
+  background: rgba(181,181,181,0.3);
+  border: 0;
+}
+
+@media (min-width:768px) {
+  .c0 {
+    margin: calc(5.6rem * 2.5) auto 5.6rem;
+  }
+}
+
+<hr
+  class="c0"
+/>
+`)
+  })
+})
