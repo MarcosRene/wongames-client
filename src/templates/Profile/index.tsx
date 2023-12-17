f9e9fa12d02c529f68ae7f0fac53833ba1f3c73c
Marcos Renê <renemoura789@gmail.com>
2023-12-17 15:47:46 -0300

    feat: add template and page Profile

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+import ProfileMenu from 'components/ProfileMenu'
+
+import Base from 'templates/Base'
+
+import { ProfileTemplateProps } from './types'
+
+import * as S from './styles'
+import { useRouter } from 'next/router'
+
+const Profile = ({ children }: ProfileTemplateProps) => {
+  const { asPath } = useRouter()
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My profile
+        </Heading>
+
+        <S.Main>
+          <ProfileMenu activeLink={asPath} />
+
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  )
+}
+
+export default Profile
---
+++ src/templates/Profile/types.tsx
@@ -0,0 +1,1 @@
+export type ProfileTemplateProps = { children: React.ReactNode }
