e25f19d35d706535369bb1c34ca55c11ea76f075
Marcos Renê <renemoura789@gmail.com>
2023-12-17 16:30:12 -0300

    test: add tests profile template

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -1,10 +1,40 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests'
 
 import Profile from '.'
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ asPath: '/profile/me' }))
+}))
+
+jest.mock('templates/Base', () => {
+  // eslint-disable-next-line react/display-name
+  return ({ children }: { children: React.ReactNode }) => {
+    return <div data-testid="base">{children}</div>
+  }
+})
+
+jest.mock('components/Heading', () => {
+  // eslint-disable-next-line react/display-name
+  return ({ children }: { children: React.ReactNode }) => {
+    return <div data-testid="heading">{children}</div>
+  }
+})
+
+jest.mock('components/ProfileMenu', () => {
+  // eslint-disable-next-line react/display-name
+  return () => {
+    return <div data-testid="profile-menu" />
+  }
+})
+
 describe('<Profile />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Profile />)
+  it('should render sections', () => {
+    renderWithTheme(<Profile>Lorem Ipsum</Profile>)
+
+    expect(screen.getByText('Lorem Ipsum')).toBeInTheDocument()
+    expect(screen.getByText('My profile')).toBeInTheDocument()
+    expect(screen.getByTestId('profile-menu')).toBeInTheDocument()
   })
 })
